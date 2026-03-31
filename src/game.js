c45b13da3a0f82a3c2d8006d140087d468cc5d94
microegor <microegor321@gmail.com>
2026-03-31 13:50:21 +0200

    feat: add a getDown function

--- src/game.js
+++ src/game.js
@@ -3,20 +3,38 @@ const bird = {
     el: document.getElementById("bird"),
 };
 
+const table = document.getElementById("table");
+
+const change = 0;
+
 function jump(e) {
     if (e.code == "Space") {
         // const sizeMatch =  /(\d+)px/.exec(bird.style.top)[1];
         // let size = parseInt(sizeMatch, 10)
-        bird.top -= 1;
+        bird.top -= 200
+
         render();
     }
-    console.log("not space")
 }
 
 function render() {
     bird.el.style.top = bird.top;
 }
 
+function getDown() {
+    if (bird.top < table.offsetHeight) {
+        bird.top += 20;
+        render();
+    } else {
+        window.location.href = "end.html";
+    }
+
+
+}
+
+setInterval(getDown, 50)
+
 render();
 
-document.addEventListener("keydown", jump)
+document.addEventListener("keyup", jump)
+
