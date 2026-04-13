6381718dda41c768b45e934d4b6229ab2c19497c
microegor <microegor321@gmail.com>
2026-04-13 17:51:57 +0200

    feat: fall and jump function added

--- Jumper/src/game.js
+++ Jumper/src/game.js
@@ -1,16 +1,18 @@
 const table = document.getElementById("table");
+const jumper = document.createElement("div");
+
+let curTopPosition = 0;
+let isGameOver = true;
+const fallSpeed = 20;
 
 function newJumper() {
-    
-    const jumper = document.createElement("div");
     jumper.classList.add("jumper");
 
     let height = 30;
     let width = 20;
 
     let positionLeft = table.offsetWidth / 2 - (width / 2);
-    let positionTop = table.offsetHeight - (table.offsetHeight / 4) + (height / 2);
-
+    let positionTop = table.offsetHeight - (table.offsetHeight / 4);
 
     jumper.style.height = height + "px";
     jumper.style.width = width + "px";
@@ -20,4 +22,34 @@ function newJumper() {
     table.appendChild(jumper);
 }
 
+function getDown() {
+    if (isGameOver) {
+        curTopPosition = parseInt(jumper.style.top);
+        curTopPosition += fallSpeed; 
+        jumper.style.top = curTopPosition + "px";
+    }
+}
+
+function jump(e){
+    if (isGameOver && e.code == "Space") {
+        curTopPosition = parseInt(jumper.style.top);
+        curTopPosition -= 500;
+        jumper.style.top = curTopPosition + "px";
+    }
+}
+
+function isColliding(a, b) {
+    const rect1 = a.getBoundingClientRect();
+    const rect2 = b.getBoundingClientRect();
+
+    return !(
+        rect1.right < rect2.left ||
+        rect1.left > rect2.right ||
+        rect1.bottom < rect2.top ||
+        rect1.top > rect2.bottom
+    );
+}
+
 newJumper();
+document.addEventListener("keyup",jump);
+setInterval(getDown, 50);
