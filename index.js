529ca4a398ef3a4bd40257e9ebcbae6b2e226273
microegor <microegor321@gmail.com>
2026-04-13 13:21:39 +0200

    feat: index changed

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const link = document.getElementById("game");
+// const link = document.getElementById("game");
 
-function moveTo() {
-    window.location.href = "bird/bird.menu.html";
-}
+// function moveTo() {
+//     window.location.href = "bird/bird.menu.html";
+// }
 
-link.addEventListener("click", moveTo)
+// link.addEventListener("click", moveTo)
