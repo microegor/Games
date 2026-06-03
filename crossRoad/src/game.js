a8fe3005fed48fdfd990e7191765d451d9330a66
microegor <microegor321@gmail.com>
2026-06-03 12:40:14 +0200

    feat: side move realisation

--- crossRoad/src/game.js
+++ crossRoad/src/game.js
@@ -3,14 +3,87 @@ const screen = document.getElementById("screen");
 const ChickenWidth = 30;
 const ChickenHeight = 30;
 
-function newChiken(){
-    const chiken = document.createElement("div");
-    chiken.classList.add("chiken");
-    chiken.style.width = ChickenWidth + "px";
-    chiken.style.height = ChickenHeight + "px";
-    chiken.style.top =  + "px";
-    
-    screen.appendChilde(chiken);
+const step = 30;      
+const speed = 5;      
+
+let chickenX = 300;
+let chickenY = 400;
+
+let targetX = chickenX;
+let targetY = chickenY;
+
+let isMoving = false;
+
+const chiken = document.createElement("div");
+chiken.classList.add("chiken");
+
+chiken.style.width = ChickenWidth + "px";
+chiken.style.height = ChickenHeight + "px";
+
+screen.appendChild(chiken);
+
+function drawChicken() {
+  chiken.style.left = chickenX + "px";
+  chiken.style.top = chickenY + "px";
 }
 
-newChiken();
+function moveToTarget() {
+  if (!isMoving) return;
+
+  if (chickenX < targetX) {
+    chickenX += speed;
+  }
+
+  if (chickenX > targetX) {
+    chickenX -= speed;
+  }
+
+  if (chickenY < targetY) {
+    chickenY += speed;
+  }
+
+  if (chickenY > targetY) {
+    chickenY -= speed;
+  }
+
+  if (Math.abs(chickenX - targetX) < speed) {
+    chickenX = targetX;
+  }
+
+  if (Math.abs(chickenY - targetY) < speed) {
+    chickenY = targetY;
+  }
+
+  drawChicken();
+
+  if (chickenX === targetX && chickenY === targetY) {
+    isMoving = false;
+    return;
+  }
+
+  requestAnimationFrame(moveToTarget);
+}
+
+document.addEventListener("keydown", function(event) {
+     if (event.repeat) return; 
+  if (isMoving) return;
+
+  if (event.key === "ArrowRight") {
+    targetX = chickenX + step;
+  }
+
+  if (event.key === "ArrowLeft") {
+    targetX = chickenX - step;
+  }
+  // Реализация движения карты
+  if (event.key === "ArrowUp") {
+  }
+
+  if (event.key === "ArrowDown") {
+  }
+
+  isMoving = true;
+  moveToTarget();
+});
+
+drawChicken();
